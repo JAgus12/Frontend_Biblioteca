0b24167e4f7cddd631fd4dede294711dfa550997
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-24 11:41:21 +0200

    registro

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { UserRegistro } from '../../models/user-registro';
 import { FormsModule } from '@angular/forms';
+import { RegistroService } from './registro.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -19,9 +21,32 @@ export class RegistroComponent {
     usuario:'',
     password:''
   }
+  comprobacion:string=''
 
+  constructor(public registroService:RegistroService){
+
+  }
+
+  route=inject(Router)
 
   registrarse(){
+    console.log(this.comprobacion)
     console.log(this.userRegistro)
+    if(this.userRegistro.password==this.comprobacion){
+    this.registroService.registroUser(this.userRegistro).subscribe({
+          next:(response)=>{
+            if(response.token){
+              this.route.navigate(['/dashboard'])
+              localStorage.setItem('token',response.token)
+            }
+          },
+          error:()=>{
+            console.log('error en registro')
+          }
+        })
+      }else{
+        alert('las contraseñas no coinciden')
+      }
+   
   }
 }
