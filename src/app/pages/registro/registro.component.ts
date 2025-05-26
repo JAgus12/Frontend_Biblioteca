fc0b0fb273edf4b73381ce797727fe7a13c9e191
JAgus12 <gonzalezjagustin42@gmail.com>
2025-05-26 18:32:29 +0200

    validaciones

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,52 +1,67 @@
 import { Component, inject } from '@angular/core';
 import { UserRegistro } from '../../models/user-registro';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RegistroService } from './registro.service';
 import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
-  imports: [FormsModule],
+  imports: [FormsModule,ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
 export class RegistroComponent {
-  userRegistro:UserRegistro={
-    nombre:'',
-    apellido1:'',
-    apellido2:'',
-    email:'',
-    telefono:'',
-    fechaNacimiento:new Date(),
-    usuario:'',
-    password:''
-  }
-  comprobacion:string=''
+  
+  constructor(public registroService:RegistroService){}
 
-  constructor(public registroService:RegistroService){
-
-  }
+  registro=new FormGroup({
+    nombre:new FormControl('',Validators.required),
+    apellido1:new FormControl('',Validators.required),
+    apellido2:new FormControl(''),
+    email:new FormControl('',[Validators.email,Validators.required]),
+    telefono:new FormControl('',Validators.required),
+    fechaNacimiento:new FormControl('',Validators.required),
+    usuario:new FormControl('',Validators.required),
+    password:new FormControl('',Validators.minLength(6)),
+    comprobacionPassword:new FormControl('',Validators.required)
+  })
 
   route=inject(Router)
 
   registrarse(){
-    console.log(this.comprobacion)
-    console.log(this.userRegistro)
-    if(this.userRegistro.password==this.comprobacion){
-    this.registroService.registroUser(this.userRegistro).subscribe({
-          next:(response)=>{
-            if(response.token){
-              this.route.navigate(['/dashboard'])
-              localStorage.setItem('token',response.token)
-            }
-          },
-          error:()=>{
-            console.log('error en registro')
-          }
-        })
-      }else{
-        alert('las contraseñas no coinciden')
-      }
+    //console.log(this.registro.value)
+    if(this.registro.valid){
+        if(this.registro.value.password==this.registro.value.comprobacionPassword){
+          const userRegistro:UserRegistro={
+                              nombre:this.registro.value.nombre??'',
+                              apellido1:this.registro.value.apellido1??'',
+                              apellido2:this.registro.value.apellido2??'',
+                              email:this.registro.value.email??'',
+                              telefono:this.registro.value.telefono??'',
+                              fechaNacimiento:new Date(this.registro.value.fechaNacimiento??''),
+                              usuario:this.registro.value.usuario??'',
+                              password:this.registro.value.password??''
+                            }
+            console.log(userRegistro)
+            this.registroService.registroUser(userRegistro).subscribe({
+                  next:(response)=>{
+                    if(response.token){
+                      this.route.navigate(['/dashboard'])
+                      localStorage.setItem('token',response.token)
+                    }
+                  },
+                  error:()=>{
+                    console.log('error en registro')
+                  }
+                })
+                }else{
+          alert('las contraseñas no coinciden')
+        }
+    }else{
+
+    }
+
+   
    
   }
 }
